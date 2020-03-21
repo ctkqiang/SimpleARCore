11729124c13d54721ffc44a2cecc5b08be721c41
John Melody Me and Tan Sin Dee <johnmelodyme@icloud.com>
2020-03-21 17:29:36 +0800

    Committed

---
+++ app/src/main/java/my/kylogger/johnmelodyme/iot/simplearcore/MainActivity.java
@@ -0,0 +1,14 @@
+package my.kylogger.johnmelodyme.iot.simplearcore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
